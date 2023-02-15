48fc922c1eec0f61560d10293cd511331d904e8e
Shobhit Goswami <shobhit.goswami@zestmoney.in>
2023-02-15 23:43:46 +0530

    Added Bottom NavBar with navigation graph, Fragments

---
+++ app/src/main/java/com/example/shopee/view/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.shopee.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.shopee.R
+import com.example.shopee.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+
+    private val navController by lazy { navHostFragment.navController }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController.setGraph(R.navigation.dashboard_navigation)
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+}
